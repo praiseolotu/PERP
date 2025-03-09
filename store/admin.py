cc364ce271da55974b925db3af2361e560e286ee
praiseolotu <olioxxayo@gmail.com>
2025-03-09 04:35:56 +0100

    initial commit

---
+++ store/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import Category, Item, Delivery
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Category model.
+    """
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Item model.
+    """
+    list_display = (
+        'name', 'category', 'quantity', 'price', 'expiring_date', 'vendor'
+    )
+    search_fields = ('name', 'category__name', 'vendor__name')
+    list_filter = ('category', 'vendor')
+    ordering = ('name',)
+
+
+class DeliveryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Delivery model.
+    """
+    list_display = (
+        'item', 'customer_name', 'phone_number',
+        'location', 'date', 'is_delivered'
+    )
+    search_fields = ('item__name', 'customer_name')
+    list_filter = ('is_delivered', 'date')
+    ordering = ('-date',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Delivery, DeliveryAdmin)
+
